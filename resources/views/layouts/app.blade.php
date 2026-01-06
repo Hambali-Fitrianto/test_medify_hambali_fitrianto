bae51ebcc156efcacfc313fee406e6782721dca0
Hambali-Fitrianto <hambali.fitrianto01@gmail.com>
2026-01-06 14:08:44 +0700

    perbaikan route

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -73,38 +73,41 @@
 
                 <div class="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul class="navbar-nav me-auto">
+                        {{-- HANYA TAMPILKAN MENU INI JIKA SUDAH LOGIN (@auth) --}}
+                        @auth
                         <li class="nav-item">
                             <a class="nav-link {{ request()->is('master-items*') ? 'active' : '' }}" href="{{ url('master-items') }}">Master Items</a>
                         </li>
                         <li class="nav-item">
                             <a class="nav-link {{ request()->is('categories*') ? 'active' : '' }}" href="{{ url('categories') }}">Kategori Items</a>
                         </li>
+                        @endauth
                     </ul>
 
                     <ul class="navbar-nav ms-auto">
                         @guest
-                        @if (Route::has('login'))
-                        <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                        @endif
-                        @if (Route::has('register'))
-                        <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
-                        @endif
+                            @if (Route::has('login'))
+                                <li class="nav-item"><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
+                            @endif
+                            @if (Route::has('register'))
+                                <li class="nav-item"><a class="nav-link" href="{{ route('register') }}">{{ __('Register') }}</a></li>
+                            @endif
                         @else
-                        <li class="nav-item dropdown">
-                            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                {{ Auth::user()->name }}
-                            </a>
-
-                            <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-                                <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
-                                    {{ __('Logout') }}
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    {{ Auth::user()->name }}
                                 </a>
 
-                                <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
-                                    @csrf
-                                </form>
-                            </div>
-                        </li>
+                                <div class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                                        {{ __('Logout') }}
+                                    </a>
+
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
                         @endguest
                     </ul>
                 </div>
@@ -117,11 +120,8 @@
     </div>
 
     <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
-
     <script src="https://cdn.jsdelivr.net/npm/select2@4.1.0-rc.0/dist/js/select2.min.js"></script>
-
     <script src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.min.js"></script>
     <script src="https://cdn.datatables.net/1.12.1/js/dataTables.bootstrap5.min.js"></script>
 
--- routes/web.php
+++ routes/web.php
@@ -13,38 +13,46 @@ use App\Http\Controllers\CategoryController;
 |--------------------------------------------------------------------------
 */
 
+// 1. UBAH BAGIAN INI: Redirect root '/' ke halaman login
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
+// Fitur Auth bawaan Laravel (Login, Register, Logout)
 Auth::routes();
 
-// Home
-Route::get('/home', [HomeController::class, 'index'])->name('home');
+// 2. TAMBAHKAN MIDDLEWARE GROUP
+// Semua route di dalam group ini HANYA bisa diakses jika user sudah LOGIN
+Route::middleware(['auth'])->group(function () {
 
-// ====================================================
-// MASTER ITEMS ROUTES
-// ====================================================
-Route::get('/master-items', [MasterItemsController::class, 'index']);
-Route::get('/master-items/search', [MasterItemsController::class, 'search']); // AJAX Search
-Route::get('/master-items/form/{method}/{id?}', [MasterItemsController::class, 'formView']);
-Route::post('/master-items/form/{method}/{id?}', [MasterItemsController::class, 'formSubmit']);
-Route::get('/master-items/view/{kode}', [MasterItemsController::class, 'singleView']);
-Route::get('/master-items/delete/{id}', [MasterItemsController::class, 'delete']);
-Route::get('/master-items/update-random-data', [MasterItemsController::class, 'updateRandomData']);
+    // Home
+    Route::get('/home', [HomeController::class, 'index'])->name('home');
 
-// Export Excel (Fitur Baru)
-Route::get('/master-items/export-excel', [MasterItemsController::class, 'exportExcel']);
+    // ====================================================
+    // MASTER ITEMS ROUTES
+    // ====================================================
+    Route::get('/master-items', [MasterItemsController::class, 'index']);
+    Route::get('/master-items/search', [MasterItemsController::class, 'search']); // AJAX Search
+    Route::get('/master-items/form/{method}/{id?}', [MasterItemsController::class, 'formView']);
+    Route::post('/master-items/form/{method}/{id?}', [MasterItemsController::class, 'formSubmit']);
+    Route::get('/master-items/view/{kode}', [MasterItemsController::class, 'singleView']);
+    Route::get('/master-items/delete/{id}', [MasterItemsController::class, 'delete']);
+    Route::get('/master-items/update-random-data', [MasterItemsController::class, 'updateRandomData']);
 
-// ====================================================
-// KATEGORI ITEMS ROUTES
-// ====================================================
-Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/search', [CategoryController::class, 'search']); // AJAX Search
-Route::get('/categories/form/{method}/{id?}', [CategoryController::class, 'formView']);
-Route::post('/categories/form/{method}/{id?}', [CategoryController::class, 'formSubmit']);
-Route::get('/categories/view/{id}', [CategoryController::class, 'singleView']);
-Route::get('/categories/delete/{id}', [CategoryController::class, 'delete']);
+    // Export Excel
+    Route::get('/master-items/export-excel', [MasterItemsController::class, 'exportExcel']);
 
-// Download PDF (Fitur Baru)
-Route::get('/categories/pdf/{id}', [CategoryController::class, 'generatePdf']);
+    // ====================================================
+    // KATEGORI ITEMS ROUTES
+    // ====================================================
+    Route::get('/categories', [CategoryController::class, 'index']);
+    Route::get('/categories/search', [CategoryController::class, 'search']); // AJAX Search
+    Route::get('/categories/form/{method}/{id?}', [CategoryController::class, 'formView']);
+    Route::post('/categories/form/{method}/{id?}', [CategoryController::class, 'formSubmit']);
+    Route::get('/categories/view/{id}', [CategoryController::class, 'singleView']);
+    Route::get('/categories/delete/{id}', [CategoryController::class, 'delete']);
+
+    // Download PDF
+    Route::get('/categories/pdf/{id}', [CategoryController::class, 'generatePdf']);
+
+});
