d6c6e8e565a07a7cfb82cf1de336e441c2b813a5
Hambali-Fitrianto <hambali.fitrianto01@gmail.com>
2026-01-06 13:51:56 +0700

    update routes

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Auth;
 // Import Controller
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MasterItemsController;
-use App\Http\Controllers\CategoryController; // Tambahkan ini
+use App\Http\Controllers\CategoryController;
 
 /*
 |--------------------------------------------------------------------------
@@ -19,27 +19,32 @@ Route::get('/', function () {
 
 Auth::routes();
 
-// Home Routes
+// Home
 Route::get('/home', [HomeController::class, 'index'])->name('home');
-// Route::get('/', [HomeController::class, 'index'])->name('home'); // Opsional jika ingin root langsung ke home
 
-// --- MASTER ITEMS ROUTES ---
+// ====================================================
+// MASTER ITEMS ROUTES
+// ====================================================
 Route::get('/master-items', [MasterItemsController::class, 'index']);
-Route::get('/master-items/search', [MasterItemsController::class, 'search']);
+Route::get('/master-items/search', [MasterItemsController::class, 'search']); // AJAX Search
 Route::get('/master-items/form/{method}/{id?}', [MasterItemsController::class, 'formView']);
 Route::post('/master-items/form/{method}/{id?}', [MasterItemsController::class, 'formSubmit']);
 Route::get('/master-items/view/{kode}', [MasterItemsController::class, 'singleView']);
 Route::get('/master-items/delete/{id}', [MasterItemsController::class, 'delete']);
 Route::get('/master-items/update-random-data', [MasterItemsController::class, 'updateRandomData']);
 
-// --- KATEGORI ITEMS ROUTES (BARU) ---
+// Export Excel (Fitur Baru)
+Route::get('/master-items/export-excel', [MasterItemsController::class, 'exportExcel']);
+
+// ====================================================
+// KATEGORI ITEMS ROUTES
+// ====================================================
 Route::get('/categories', [CategoryController::class, 'index']);
-Route::get('/categories/search', [CategoryController::class, 'search']);
+Route::get('/categories/search', [CategoryController::class, 'search']); // AJAX Search
 Route::get('/categories/form/{method}/{id?}', [CategoryController::class, 'formView']);
 Route::post('/categories/form/{method}/{id?}', [CategoryController::class, 'formSubmit']);
 Route::get('/categories/view/{id}', [CategoryController::class, 'singleView']);
 Route::get('/categories/delete/{id}', [CategoryController::class, 'delete']);
-// Route Download PDF
-Route::get('categories/pdf/{id}', [CategoryController::class, 'generatePdf']);
-// Route Export Excel
-Route::get('master-items/export-excel', [MasterItemsController::class, 'exportExcel']);
+
+// Download PDF (Fitur Baru)
+Route::get('/categories/pdf/{id}', [CategoryController::class, 'generatePdf']);
